e7e6b63e1bd46a62042ab0c0f146390994f1c730
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2024-01-18 12:52:25 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
+declare function init([]): any;
+declare function animationInit([]): any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'nettlifyflix';
+  /**
+   *
+   */
+  constructor() {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      init($);
+      animationInit($);
+    }, 50);
+  }
 }
